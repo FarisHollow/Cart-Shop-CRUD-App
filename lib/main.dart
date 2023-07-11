474fd6f3dfa28c4ab12596edb1add43d4fd0b458
FarisHollow <mirfaris79@gmail.com>
2023-07-12 00:22:22 +0600

    Cart Order Shop

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          inputDecorationTheme: const InputDecorationTheme(
+            border: OutlineInputBorder(),
+          )
+      ),
+    );
+  }
+}
